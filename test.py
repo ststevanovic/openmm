68acd8c6fe5fcaadd7c118e2062c849014c6fefb
ststevanovic <st.stevanov@gmail.com>
2025-12-25 22:34:13 +0100

    real cuda test

--- test.py
+++ test.py
@@ -5,15 +5,21 @@ repo_root = Path(__file__).resolve().parent
 script = repo_root / "examples" / "benchmarks" / "benchmark.py"
 cwd = str(script.parent)
 
-print("Running CUDA benchmark...")
+print("Running CUDA benchmark...", flush=True)
 subprocess.run(
-    ["python", str(script), "--platform=Reference", "--style=table", "--outfile=double.yaml"], 
+    ["python", str(script), "--platform=CUDA", "--precision=mixed", "--outfile=cuda-mixed.yaml"], 
     cwd=cwd,
     check=True)
 
-print("Running CPU benchmark...")
-subprocess.run(
-    ["python", str(script), "--platform=CPU", "--test=apoa1pme", "--pme-cutoff=0.8","--outfile=cpu-mixed.yaml"], 
-    cwd=cwd,
-    check=True)
+# print("Running Reference benchmark...")
+# subprocess.run(
+#     ["python", str(script), "--platform=Reference", "--style=table", "--outfile=double.yaml"], 
+#     cwd=cwd,
+#     check=True)
+
+# print("Running CPU benchmark...")
+# subprocess.run(
+#     ["python", str(script), "--platform=CPU", "--test=apoa1pme", "--pme-cutoff=0.8","--outfile=cpu-mixed.yaml"], 
+#     cwd=cwd,
+#     check=True)
 
